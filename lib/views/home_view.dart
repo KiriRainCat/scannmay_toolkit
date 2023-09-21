88f9d55ab2599c34fb2ec1798a7b80562dcbdfb0
KiriRainCat <2287990465@qq.com>
2023-09-21 14:29:00 +0800

    ✨ feat(主页): 添加三个常用网站的快捷按钮

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:scannmay_toolkit/components/rounded_button.dart';
+import 'package:scannmay_toolkit/functions/utils/utils.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -8,9 +10,32 @@ class HomeView extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(
-          "主页",
-          style: Theme.of(context).textTheme.displaySmall,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RoundedButton(
+              width: 64,
+              height: 64,
+              onPressed: () => Utils.openUrl("https://login.jupitered.com/login/"),
+              buttonContent: Card(child: Image.asset("assets/images/icon_jupiter.png")),
+            ),
+            const SizedBox(width: 8),
+            RoundedButton(
+              width: 64,
+              height: 64,
+              onPressed: () => Utils.openUrl("https://outlook.office.com/mail/"),
+              buttonContent: Card(
+                child: Image.asset("assets/images/icon_outlook.png"),
+              ),
+            ),
+            const SizedBox(width: 8),
+            RoundedButton(
+              width: 64,
+              height: 64,
+              onPressed: () => Utils.openUrl("https://teams.microsoft.com/"),
+              buttonContent: Card(child: Image.asset("assets/images/icon_teams.png")),
+            ),
+          ],
         ),
       ],
     );
