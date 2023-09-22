345a60336f9c3138e3611fd9be2e0777c0dc2c99
KiriRainCat <2287990465@qq.com>
2023-09-22 16:07:34 +0800

    ✨ feat: 添加查看作业详情的按钮，并添加作业信息展示弹窗

--- lib/views/notification_view/notification_card.dart
+++ lib/views/notification_view/notification_card.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:isar/isar.dart';
+import 'package:scannmay_toolkit/components/rounded_button.dart';
+import 'package:scannmay_toolkit/functions/assignment_notifier/bg_worker.dart';
+import 'package:scannmay_toolkit/model/jupiter.dart';
 
 import 'package:scannmay_toolkit/model/notification.dart';
 import 'package:scannmay_toolkit/functions/utils/utils.dart';
@@ -18,6 +23,61 @@ class NotificationCard extends StatelessWidget {
     if (parts[0].length < 2) parts[0] = "0${parts[0]}";
     if (parts[1].length < 2) parts[1] = "0${parts[1]}";
     return parts.join("/");
+  }
+
+  void showAssignmentInfo(Assignment assignment) async {
+    final data = await AssignmentNotifierBgWorker.isar.jupiterDatas.filter().idEqualTo(0).findFirst();
+    final course = data!.courses!.firstWhere((course) => course.name == message.course);
+    var desc = course.assignments!.firstWhere((item) => item.title == assignment.title).desc;
+
+    if (desc == null || desc.isEmpty) desc = "None";
+    desc = desc.replaceFirst(RegExp(r'Directions\n'), "");
+    desc = desc.split("\n").join("\n\n");
+
+    Get.dialog(
+      Dialog(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                RoundedButton(
+                  onPressed: () => Get.back(),
+                  buttonContent: Icon(Icons.close, size: 18, color: Colors.red.shade900),
+                  margin: const EdgeInsets.fromLTRB(0, 8, 10, 0),
+                  width: 20,
+                  height: 20,
+                ),
+              ],
+            ),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(8, 0, 8, 24),
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Row(), // 占位符，用于撑满整个 Column 的水平轴
+                      Text(assignment.title!, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+                      const SizedBox(height: 4),
+                      Text("From: ${message.course}"),
+                      const SizedBox(height: 4),
+                      Text("Due: ${formatDueDate(assignment.due!)}"),
+                      const SizedBox(height: 4),
+                      Text("Score: [ ${assignment.score} ]"),
+                      const SizedBox(height: 16),
+                      const Text("Directions: ", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                      Text(desc),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
@@ -73,6 +133,18 @@ class NotificationCard extends StatelessWidget {
                       ),
                       const SizedBox(width: 12),
                       Text("[ ${assignment.score!} ]"),
+                      const SizedBox(width: 4),
+                      Column(
+                        children: [
+                          const SizedBox(height: 2),
+                          RoundedButton(
+                            width: 16,
+                            height: 16,
+                            onPressed: () => showAssignmentInfo(assignment),
+                            buttonContent: const Icon(Icons.info_outline, size: 16, color: Colors.blue),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ]
